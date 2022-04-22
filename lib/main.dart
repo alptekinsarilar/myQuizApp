9a6b63cd8695ab03edf15b51db8fb0645a176ea7
Alptekin <alptekinsarilar@gmail.com>
2022-04-22 22:31:05 +0300

    Adding layout widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -9,8 +10,27 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Başlik ZORT'),
         ),
-        body: Text('ZIrt GÖVDE'),
+        body: Column(
+          children: [
+            Text("The question"),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Yukselen buton 1 zort'),
+              hoverColor: Color.fromARGB(174, 14, 242, 128),
+              color: Color.fromARGB(174, 14, 242, 128),
+              focusColor:  Color.fromARGB(174, 14, 242, 128),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Yukselen buton 2 zort'),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text('Yukselen buton 3 zort'),
+            ),
+          ],
+        ),
       ),
-    ); 
+    );
   }
 }
