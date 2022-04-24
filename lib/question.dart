deaa311521427fbdc458195aa829a10c4de42ff1
Alptekin <alptekinsarilar@gmail.com>
2022-04-25 02:27:08 +0300

    Layouting and Styling

---
+++ lib/question.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+  const Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
